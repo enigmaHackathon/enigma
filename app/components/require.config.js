76c1c022c0e42c84cadde1c48765b729a05d047c
unknown <banajit@visibleaccent.com>
2015-01-24 13:42:59 +0530

    #added bower compontnts

--- app/components/require.config.js
+++ app/components/require.config.js
@@ -3,7 +3,10 @@ require.config({
   paths: {
     angular:"../vendor/angular/angular",
     $:"../vendor/jquery/jquery",
-    _:"../vendor/underscore/underscore"
+   // _:"../vendor/underscore/underscore",
+    d3:"../bower_components/d3/d3",
+    nvd3:"../bower_components/nvd3/nv.d3",
+    angular_nvd3_directives: "../bower_components/angularjs-nvd3-directives/dist/angularjs-nvd3-directives"
   },
   waitSeconds: 30
 });
